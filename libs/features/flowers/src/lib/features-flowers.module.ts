72f8cb45bd2c456060144ad858424b6299b28e19
lindajoy <wawirajoy98@gmail.com>
2022-04-28 08:45:43 +0300

    📦📍Added routes

--- libs/features/flowers/src/lib/features-flowers.module.ts
+++ libs/features/flowers/src/lib/features-flowers.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlowersHomeComponent } from './components/flowers-home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlowersDetailsComponent } from './pages/flowers-detail.page';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FlowersHomeComponent
+  },
+  {
+    path: ':id',
+    component: FlowersDetailsComponent
+  }
+]
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,  MatToolbarModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatCardModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ FlowersHomeComponent, FlowersDetailsComponent],
 })
 export class FeaturesFlowersModule {}
