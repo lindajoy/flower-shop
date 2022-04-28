613e49573af4da3f8b35dca1f82ac96563610dcd
lindajoy <wawirajoy98@gmail.com>
2022-04-28 08:49:37 +0300

    📦Added angular fire module

--- apps/flowershop/src/app/app.module.ts
+++ apps/flowershop/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../routes/routes.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule,
+            AngularFireModule.initializeApp(environment.firebase),
+            AngularFirestoreModule,
+            AngularFireModule],
   providers: [],
   bootstrap: [AppComponent],
 })
