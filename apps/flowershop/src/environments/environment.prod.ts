e56f3c10756b4d95d9ef7ce2b6e737fe6613b322
lindajoy <wawirajoy98@gmail.com>
2022-04-28 08:47:38 +0300

    🛠Updated environment configs

--- apps/flowershop/src/environments/environment.prod.ts
+++ apps/flowershop/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase :{
+  apiKey: "AIzaSyCEZk5iJFSYn49IXXukm4pRvP0SdWnSobw",
+  authDomain: "flower-shop-45407.firebaseapp.com",
+  projectId: "flower-shop-45407",
+  storageBucket: "flower-shop-45407.appspot.com",
+  messagingSenderId: "384971216731",
+  appId: "1:384971216731:web:f3cb61db961459a009ac7e",
+  measurementId: "G-ZX2PT611V3",
+  production: true 
+}
 };
--- apps/flowershop/src/environments/environment.ts
+++ apps/flowershop/src/environments/environment.ts
@@ -1,9 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+    production: true,
+    firebase :{
+    apiKey: "AIzaSyCEZk5iJFSYn49IXXukm4pRvP0SdWnSobw",
+    authDomain: "flower-shop-45407.firebaseapp.com",
+    projectId: "flower-shop-45407",
+    storageBucket: "flower-shop-45407.appspot.com",
+    messagingSenderId: "384971216731",
+    appId: "1:384971216731:web:f3cb61db961459a009ac7e",
+    measurementId: "G-ZX2PT611V3",
+    production: true 
+}
 };
 
 /*
